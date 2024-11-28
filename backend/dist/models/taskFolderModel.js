2a54fe6a0c71f97649a9d07b0447c59e47a877d7
preykriti <prakriteeshrestha2@gmail.com>
2024-11-28 18:27:28 +0545

    task folder model

--- backend/dist/models/taskFolderModel.js
+++ backend/dist/models/taskFolderModel.js
@@ -6,6 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.taskFolderModel = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 const taskFolderSchema = new mongoose_1.default.Schema({
-    name: { type: String, required: true }
+    name: { type: String, required: true },
+    user: { type: mongoose_1.default.Schema.Types.ObjectId, required: true, ref: "user" },
+    tasks: [{ type: mongoose_1.default.Schema.Types.ObjectId, ref: "task" }],
+    createdAt: { type: Date, default: Date.now }
 });
 exports.taskFolderModel = mongoose_1.default.model("taskFolder", taskFolderSchema);
--- backend/src/models/taskFolderModel.ts
+++ backend/src/models/taskFolderModel.ts
@@ -8,7 +8,10 @@ export interface ITaskFolder {
 }
 
 const taskFolderSchema: Schema<ITaskFolder> = new mongoose.Schema({
-    name:{type: String, required: true}
+  name: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, required: true, ref:"user" },
+  tasks: [{ type: mongoose.Schema.Types.ObjectId, ref:"task" }],
+  createdAt: {type:Date, default:Date.now}
 });
 
 export const taskFolderModel: Model<ITaskFolder> = mongoose.model("taskFolder", taskFolderSchema);
