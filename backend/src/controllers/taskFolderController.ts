167b67f8f211a99f5c6cd4f6fb3d191307b40442
preykriti <prakriteeshrestha2@gmail.com>
2024-12-02 20:29:33 +0545

    get a folder

--- backend/src/controllers/taskFolderController.ts
+++ backend/src/controllers/taskFolderController.ts
@@ -1,60 +1,152 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import { ITaskFolder, taskFolderModel } from "../models/taskFolderModel";
 import { CustomJwtPayload } from "../types/types";
 import { validationResult } from "express-validator";
+import { taskModel } from "../models/taskModel";
+import mongoose from "mongoose";
 
 // ! to get all folders
-export const getAllFolders = async(req: Request, res:Response): Promise<void>=>{
-    try {
-        let user = req.user as CustomJwtPayload | undefined;
-        const userID = user?.id;
-        if(!userID){
-             res
-               .status(400)
-               .json({ success: false, message: "Unauthorized user." });
-             return;
-        }
-        const folders = await taskFolderModel.find({user: userID}).populate("name");
-        res.status(200).json({success:true, folders})
-    } catch (error) {
-        console.log(error);
-        res
-          .status(500)
-          .json({ success: false, message: "Internal server error" });
-        return;
-    }
-
-}
-
-// ! to create a folder
-export const createFolder = async(req: Request, res: Response):Promise<void>=>{
-    const result = validationResult(req);
-    if (!result.isEmpty()) {
-      res.status(400).json({ success: false, errors: result.array() });
+export const getAllFolders = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    let user = req.user as CustomJwtPayload | undefined;
+    const userID = user?.id;
+    if (!userID) {
+      res.status(400).json({ success: false, message: "Unauthorized user." });
       return;
     }
-    try {
-        const{name} = req.body;
-        let user = req.user as CustomJwtPayload | undefined;
-        const userID = user?.id;
-        if(!userID){
-            res.status(404).json({success:false, message: "Unauthorized user."});
-            return;
-        }
-        const folder:ITaskFolder | null = await taskFolderModel.findOne({user:userID, name});
-        if(folder){
-            res.status(400).json({success:false, message:"Folder name already exists."});
-            return;
-        }
-        const createdFolder = await taskFolderModel.create({name, user:userID});
-        res.status(200).json({success:true, createdFolder})
-        
-    } catch (error) {
-        console.log(error);
-        res
-          .status(500)
-          .json({ success: false, message: "Internal server error" });
-        return;
-    }
-}
+    const folders = await taskFolderModel.find({ user: userID });
+    res.status(200).json({ success: true, folders });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+    return;
+  }
+};
 
+// ! to create a folder
+export const createFolder = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const result = validationResult(req);
+  if (!result.isEmpty()) {
+    res.status(400).json({ success: false, errors: result.array() });
+    return;
+  }
+  try {
+    const { name } = req.body;
+    let user = req.user as CustomJwtPayload | undefined;
+    const userID = user?.id;
+    if (!userID) {
+      res.status(404).json({ success: false, message: "Unauthorized user." });
+      return;
+    }
+    const folder: ITaskFolder | null = await taskFolderModel.findOne({
+      user: userID,
+      name,
+    });
+    if (folder) {
+      res
+        .status(400)
+        .json({ success: false, message: "Folder name already exists." });
+      return;
+    }
+    const createdFolder = await taskFolderModel.create({ name, user: userID });
+    res.status(200).json({ success: true, createdFolder });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+    return;
+  }
+};
+
+// ! delete a folder
+export const deleteFolder = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const user = req.user as CustomJwtPayload | undefined;
+    const userID = user?.id;
+    if (!userID) {
+      res.status(401).json({ success: false, message: "Unauthorized user." });
+      return;
+    }
+    const folderID = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(folderID)) {
+      res.status(400).json({ success: false, message: "Invalid folder ID." });
+      return;
+    }
+    const folder = await taskFolderModel.findOne({
+      _id: folderID,
+      user: userID,
+    });
+    if (!folder) {
+      res.status(400).json({ success: false, message: "Folder not found" });
+      return;
+    }
+
+    // creating session to maintain atomicity
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    try {
+      await taskModel.deleteMany({ _id: { $in: folder.tasks } });
+
+      await taskFolderModel.deleteOne({ user: userID, _id: folderID });
+      await session.commitTransaction();
+      res
+        .status(200)
+        .json({ success: true, message: "Folder deleted successfully." });
+    } catch (error) {
+      await session.abortTransaction();
+      console.log(error);
+      res.status(500).json({
+        success: false,
+        message: "Failed to delete folder and tasks.",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+    return;
+  }
+};
+
+// ! get one folder by id
+export const getOneFolder = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const user = req.user as CustomJwtPayload | undefined;
+    const userID = user?.id;
+    if (!userID) {
+      res.status(401).json({ success: false, message: "Unauthorized user." });
+      return;
+    }
+
+    const folderID = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(folderID)) {
+      res.status(400).json({ success: false, message: "Invalid folder ID." });
+      return;
+    }
+    const folder = await taskFolderModel.findOne({
+      _id: folderID,
+      user: userID,
+    }).populate("tasks","title");
+    if (!folder) {
+      res.status(400).json({ success: false, message: "Folder not found" });
+      return;
+    }
+    res.status(200).json({success:true, data: folder })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+    return;
+  }
+};
+
+// ! update a folder
--- backend/src/routes/taskFolderRoute.ts
+++ backend/src/routes/taskFolderRoute.ts
@@ -1,11 +1,13 @@
 import { fetchUser } from './../middlewares/fetchUser';
 import express from "express";
-import { createFolder, getAllFolders } from "../controllers/taskFolderController";
+import { createFolder, deleteFolder, getAllFolders, getOneFolder } from "../controllers/taskFolderController";
 import { folderValidationRule } from '../middlewares/validationRules';
 
 const taskFolderRouter = express.Router();
 
 taskFolderRouter.get("/getallfolders",fetchUser, getAllFolders);
 taskFolderRouter.post("/createfolder", folderValidationRule,fetchUser, createFolder);
+taskFolderRouter.delete("/deletefolder/:id", fetchUser, deleteFolder);
+taskFolderRouter.get("/getonefolder/:id", fetchUser, getOneFolder);
 
 export default taskFolderRouter;
