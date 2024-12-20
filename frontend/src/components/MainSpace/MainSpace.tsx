9be1d81e20e17df360853a2ac884693d0e09dc4e
preykriti <prakriteeshrestha2@gmail.com>
2024-12-20 18:12:10 +0545

    ui

--- frontend/src/components/MainSpace/MainSpace.tsx
+++ frontend/src/components/MainSpace/MainSpace.tsx
@@ -4,19 +4,22 @@ import "./MainSpace.css";
 const MainSpace = () => {
   return (
     <div className="main-space">
-      <Task title="This is my first task" />
-      <Task title="This is my another task" />
-      <Task title="This is my another task" />
-      <Task title="This is my another task" />
-      <Task title="This is my another task" />
-      <Task title="This is my another task" />
-      <Task title="This is my another task" />
-      <Task title="This is my another task" />
-      <Task title="This is my another task" />
-      <Task title="This is my another task" />
-      <Task title="This is my another task" />
-      <Task title="This is my another task" />
-      <Task title="This is my second task hwof at ai am a it che faohow a foa r thsi sdo es it hso fisa thoguoaigh aghaoghag agoahgodhgoaoghskfghaghairghairugha ruahrg arhgiuagh agh aghaiguhigsfhaeifh aertu haprt aerpo geari eri r tieruytier ytreitypireutyprew tpreptyyerpt erpt pery pre ypte p rtp yerpy rp rtp repdfy oag oagyoaygoay yga yg aogoag oagoagog g aaghag " />
+      <div className="task-list">
+        <Task title="This is my first task" />
+        <Task title="This is my another task" />
+        <Task title="This is my another task" />
+        <Task title="This is my another task" />
+        <Task title="This is my another task" />
+        <Task title="This is my another task" />
+        <Task title="This is my another task" />
+        <Task title="This is my another task" />
+        <Task title="This is my another task" />
+        <Task title="This is my another task" />
+        <Task title="This is my another task" />
+        <Task title="This is my another task" />
+        <Task title="This is my second task hwof at ai am a it che faohow a foa r thsi sdo es it hso fisa thoguoaigh aghaoghag agoahgodhgoaoghskfghaghairghairugha ruahrg arhgiuagh agh aghaiguhigsfhaeifh aertu haprt aerpo geari eri r tieruytier ytreitypireutyprew tpreptyyerpt erpt pery pre ypte p rtp yerpy rp rtp repdfy oag oagyoaygoay yga yg aogoag oagoagog g aaghag " />
+      </div>
+      <button className="add-task-button">+</button>
     </div>
   );
 };
--- frontend/src/components/TaskDetailBar/TaskDetailBar.tsx
+++ frontend/src/components/TaskDetailBar/TaskDetailBar.tsx
@@ -3,6 +3,8 @@ import Task from "../Task/Task"
 import "./TaskDetailBar.css"
 import { MdFavorite } from "react-icons/md";
 import { MdFavoriteBorder } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+import { RxCross2 } from "react-icons/rx";
 
 const TaskDetailBar = () => {
     const [isFavourite, setIsFavourite] = useState(false);
@@ -12,23 +14,43 @@ const TaskDetailBar = () => {
   return (
     <div className="detail-bar-box">
       <div className="detail-bar">
+        <div className="top-container">
+          <div className="delete-container">
+            <MdDelete />
+          </div>
+          <div className="close-container">
+            <RxCross2 />
+          </div>
+        </div>
+
         <Task title="detail bar task" />
-        <p onClick={toggleFavorite} className="favourite-container">
-          Favourite{" "}
+
+        <div onClick={toggleFavorite} className="favourite-container">
+          Favourite
           {isFavourite ? (
-            <MdFavorite className="heart filled" />
+            <MdFavorite className="heart" />
           ) : (
-            <MdFavoriteBorder className="heart outline" />
+            <MdFavoriteBorder className="heart" />
           )}
-        </p>
+        </div>
+
         <div className="description-box">
           <p>Description</p>
           <textarea placeholder="Enter description"></textarea>
         </div>
+
         <div className="deadline">
           <p>Deadline</p>
           <input type="date"></input>
         </div>
+
+
+        <label htmlFor="folder-name">Select Folder</label>
+        <select className="folder-name">
+          <option value="general"> General</option>
+          <option value="work"> Work</option>
+          <option value="add-new">+ Add new</option>
+        </select>
       </div>
       <div className="buttons">
         <button>Save</button>
